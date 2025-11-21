ebe9922bf13ecbb5c747fe664bc3bb2f31880ba3
anushkapolapally <polapallyanushka@gmail.com>
2025-11-21 09:56:50 -0600

    a little bit of instructions

---
+++ BoardGames/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+/* 
+I'm thinking of building a collection of 3D games so that the user can
+select what game they willl be playing. I will first build dominos.
+
+Dominos start:
+- scene of table with the pool of dominos in the center
+- 28 tiles total in the boneyard (total set of dominos)
+    - 
+- Each player draws 7 tiles
+
+
+
+*/
+public class GameManager : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Debug.Log("Game Started");
+    }
+
+    void Update()
+    {
+        
+    }
+}
