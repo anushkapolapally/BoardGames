9eabe9fa8458419555700c45658f04caa1489efe
anushkapolapally <polapallyanushka@gmail.com>
2026-02-13 09:51:30 -0600

    initial state setup and makeing scripts

---
+++ BoardGame2/Assets/Scripts/BattleGamemanager.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BattleGamemanager : MonoBehaviour
+{
+
+    public List<GameObject> player1ships = new List<GameObject>();
+    public List<GameObject> player2ships = new List<GameObject>();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ BoardGame2/Assets/Scripts/BattleShip.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class BattleShip : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("mouse is down on the piece");
+    }
+
+    private void OnMouseDrag()
+    {
+        Debug.Log("mouse is down on the piece 2");
+    }
+}
