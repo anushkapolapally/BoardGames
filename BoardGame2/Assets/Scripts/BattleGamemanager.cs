1119d3e01de23fc79b35d41c617bae0b4d512da1
anushkapolapally <polapallyanushka@gmail.com>
2026-02-17 09:53:35 -0600

    placing dominos progress

--- BoardGame2/Assets/Scripts/BattleGamemanager.cs
+++ BoardGame2/Assets/Scripts/BattleGamemanager.cs
@@ -1,22 +1,81 @@
+using System;
 using System.Collections.Generic;
+using TreeEditor;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 
 public class BattleGamemanager : MonoBehaviour
 {
+    public int turn = 0;
+    private bool startedGame = false;
 
     public List<GameObject> player1ships = new List<GameObject>();
     public List<GameObject> player2ships = new List<GameObject>();
 
+    [SerializeField] Text initalInstructions;
+
+    [SerializeField] Button upButton;
+    [SerializeField] Button rightButton;
+    [SerializeField] Button downButton;
+    [SerializeField] Button leftButton;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        initalInstructions.text = "";
+
+        Button up = upButton.GetComponent<Button>();
+        //up.onClick.AddListener(changeUp());
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (startedGame == false)
+        {
+            startGame();
+        }
     }
+
+
+    private void startGame()
+    {
+        if (turn == 0)
+        {
+            initalInstructions.text = "Place your ships in their starting positions. Click a ship to select it and then type the row and column you would like the bottomest/leftmost part of the ship to be on.";
+
+            for(int i = 0; i < player1ships.Count; i++)
+            {
+                if (player1ships[i].GetComponent<Ship>().getClicked() == true)
+                {
+                    placingPieces(player1ships[i]);
+
+                }
+            }
+            //player 1 places pieces
+
+        }
+        else if (turn == 1)
+        {
+            //player 2 places pieces
+            startedGame = true;
+        }
+    }
+
+    private void placingPieces(GameObject ship)
+    {
+       
+    }
+
+    //button method
+
+   
+
+
+
 }
---
+++ BoardGame2/Assets/Scripts/Ship.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+
+public class Ship : MonoBehaviour
+{
+
+    public bool clicked = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        
+        if (clicked == true)
+        {
+            clicked = false;
+        }
+        else if (clicked == false)
+        {
+
+            clicked = true;
+        }
+    }
+
+    public bool getClicked()
+    {
+        return clicked;
+    }
+}
